10d1ad418a9fce82ef76c2e972fdee62528f8bca
traceofwind <gary@traceofwind.com>
2018-03-26 16:38:33 +0100

    Lodash installed. Browserify global. Bundle.js. console.log('lodash', _.without(array, 3));

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-/* to use this line we need browserify */
+/*
 import { without } from 'lodash';
-
-console.log(without);
+console.log(without); 
+browserify script.js > bundle.js
+*/
+var _ = require('lodash');
+var array = [1,2,3,4,5,6,7,8];
+console.log('lodash answer', _.without(array, 3));
+/* without method < give it array var, arguments = what you want it without */
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
